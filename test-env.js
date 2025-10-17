05b5780f1f10c2d20906e59e928be974c4281568
hakim <ai.hakim.13110@gmail.com>
2025-10-17 22:31:08 +0200

    Delete test-env.js

--- test-env.js
+++
@@ -1,9 +0,0 @@
-// api/test-env.js - Pour vérifier les variables d'environnement
-export default async function handler(req, res) {
-  res.json({
-    groqKeyPresent: !!process.env.GROQ_API_KEY,
-    groqKeyLength: process.env.GROQ_API_KEY ? process.env.GROQ_API_KEY.length : 0,
-    groqKeyStart: process.env.GROQ_API_KEY ? process.env.GROQ_API_KEY.substring(0, 10) + '...' : 'null',
-    allEnvVars: Object.keys(process.env)
-  });
-}
